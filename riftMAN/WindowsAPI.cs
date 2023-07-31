e6b39a6fdabc002ee8d37df78d3a698e5c015bf9
king_dedede <alexjbrokaw@gmail.com>
2023-07-30 17:38:53 -0700

    windowsapi refactoring

--- riftMAN/WindowsAPI.cs
+++ riftMAN/WindowsAPI.cs
@@ -5,24 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
-namespace riftMAN
+namespace riftMAN;
+
+// Methods to access the windows API for reading/writing memory to other applications.
+public static partial class WindowsAPI
 {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool ReadProcessMemory(nint hProcess, nint lpBaseAddress, [Out] byte[] lpBuffer,
+    nuint nSize, out nuint lpNumberOfBytesRead);
 
-    // Methods to access the windows API for reading/writing memory to other applications.
-    public static partial class WindowsAPI
-    {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer,
-        UIntPtr nSize, out UIntPtr lpNumberOfBytesRead);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool WriteProcessMemory(nint hProcess, nint lpBaseAddress, byte[] lpBuffer,
+    nuint nSize, out nuint lpNumberOfBytesWritten);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer,
-        UIntPtr nSize, out UIntPtr lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll")]
-        [return: MarshalAs(UnmanagedType.U4)]
-        public static extern uint GetLastError();
-    }
+    [DllImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.U4)]
+    public static extern uint GetLastError();
 }
